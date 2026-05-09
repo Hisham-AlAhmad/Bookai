7827466d7ad7b706e17654b92f7e119634dc3d1b
Hisham Al Ahmad <hisham000x@gmail.com>
2026-05-09 18:37:45 +0300

    Adding Address in the public page

--- src/app/(public)/[slug]/page.js
+++ src/app/(public)/[slug]/page.js
@@ -92,7 +92,7 @@ export default async function PublicBookingPage({ params }) {
                     <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7z" />
                     <circle cx="12" cy="9" r="2.5" />
                   </svg>
-                  {business.city}
+                  {business.city}, {business.address}
                 </span>
               )}
               {business.phone && (
